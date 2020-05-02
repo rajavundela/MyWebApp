8436eb31245f7bd134b6f23c5c647223bfa10c9d
Raja R V <rajavundela1043@gmail.com>
2020-05-02 14:09:42 +0530

    Change Layout css and display Content(F)

--- MyWebApp/Pages/Index.cshtml.cs
+++ MyWebApp/Pages/Index.cshtml.cs
@@ -5,7 +5,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Configuration.AzureKeyVault;
 using Microsoft.Data.SqlClient;
 
 namespace MyWebApp.Pages
--- MyWebApp/Pages/Portfolio.cshtml.cs
+++ MyWebApp/Pages/Portfolio.cshtml.cs
@@ -5,9 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace MyWebApp.Pages.Portfolio
+namespace MyWebApp.Pages
 {
-    public class IndexModel : PageModel
+    public class PortfolioModel : PageModel
     {
         public void OnGet()
         {
